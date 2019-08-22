f8e18710828d1095761c89ad9a3036562d8cec58
evgenik <kozyr.e@gmx.de>
2019-08-22 14:41:14 +0200

    CameraController first method with Postman implemented

--- src/main/java/aw/krauterkiste/kraeuterkisteserver/CameraController.java
+++ src/main/java/aw/krauterkiste/kraeuterkisteserver/CameraController.java
@@ -3,7 +3,15 @@ package aw.krauterkiste.kraeuterkisteserver;
 import aw.krauterkiste.kraeuterkisteserver.repositories.PicturesRepository;
 import aw.krauterkiste.kraeuterkisteserver.responsebodies.PicturesResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @RestController
 public class CameraController {
@@ -17,16 +25,31 @@ public class CameraController {
 
     // object returned by the method should be marshalled directly to the HTTP response body
 
-    @PostMapping(value = "/showPhoto")
-    @ResponseBody
-    public PicturesResponseBody showPicture() {
+//    @PostMapping(value = "/upload")
+//    @ResponseBody
+//    public PicturesResponseBody showPicture() {
+//
+//        PicturesResponseBody picturesResponseBody = new PicturesResponseBody();
+//
+//        picturesResponseBody.setPicturesList(picturesRepository.findAll());
+//
+//        return picturesResponseBody;
+//    }
 
-        PicturesResponseBody picturesResponseBody = new PicturesResponseBody();
 
-        picturesResponseBody.setPicturesList(picturesRepository.findAll());
-
-        return picturesResponseBody;
+    // this method takes the file that was given as a payload by the Client and stores it at a chosen place
+    @CrossOrigin(origins = "http://localhost:5555", allowCredentials = "true")
+    @PostMapping(value="/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        //                                            path where the picture should be saved
+        File convertFile = new File("C:\\Users\\kozyr\\Desktop\\pictureR\\"+file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fout = new FileOutputStream(convertFile);
+        fout.write(file.getBytes());
+        fout.close();
+        return new ResponseEntity<>("File is uploaded succressfully aufm Desktop", HttpStatus.OK);
     }
-
-
 }
+
+
+
