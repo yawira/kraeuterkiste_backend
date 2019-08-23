25a1569ed6ceb4de25a2130c1d0e8dee74ffec4a
yawira <yawira@mail.com>
2019-08-23 14:18:38 +0200

    20190823 Image Upload

--- src/main/java/aw/krauterkiste/kraeuterkisteserver/CameraController.java
+++ src/main/java/aw/krauterkiste/kraeuterkisteserver/CameraController.java
@@ -8,10 +8,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 @RestController
 public class CameraController {
@@ -39,17 +44,33 @@ public class CameraController {
 
     // this method takes the file that was given as a payload by the Client and stores it at a chosen place
     @CrossOrigin(origins = "http://localhost:5555", allowCredentials = "true")
-    @PostMapping(value="/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-        //                                            path where the picture should be saved
-        File convertFile = new File("C:\\Users\\kozyr\\Desktop\\pictureR\\"+file.getOriginalFilename());
+        // path where the picture should be saved
+        File convertFile = new File("C:\\Users\\" + file.getOriginalFilename());
         convertFile.createNewFile();
         FileOutputStream fout = new FileOutputStream(convertFile);
         fout.write(file.getBytes());
         fout.close();
         return new ResponseEntity<>("File is uploaded succressfully aufm Desktop", HttpStatus.OK);
     }
+
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+    @GetMapping(value = "/readImage")
+    @ResponseBody()
+    public PicturesResponseBody readImage() throws IOException {
+
+        PicturesResponseBody picturesResponseBody = new PicturesResponseBody();
+
+        Path path = Paths.get("C:\\Users\\IMG_2303.jpg");
+        byte[] picture = Files.readAllBytes(path);
+
+        String encodedImage = Base64.encode(picture);
+
+        picturesResponseBody.setEncodedImage(encodedImage);
+
+        return picturesResponseBody;
+
+    }
 }
-
-
 
