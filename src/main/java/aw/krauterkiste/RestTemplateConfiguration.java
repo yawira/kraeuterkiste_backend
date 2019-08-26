5e4e0f9b55ecce2dbbcb72aeb8c3a589a88737dd
Lorenz Josten <lorenz.josten@gmail.com>
2019-08-26 16:24:18 +0200

    edit RestTemplateConfiguration set url properties for connection to Raspberry

---
+++ src/main/java/aw/krauterkiste/RestTemplateConfiguration.java
@@ -0,0 +1,27 @@
+package aw.krauterkiste;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Value("${raspi.url}")
+    private String raspiURL;
+
+    @Bean
+    @Autowired
+    public RestTemplate raspiRestTemplate(RestTemplateBuilder builder) {
+        return raspiRestTemplateBuilder(builder);
+    }
+
+    public RestTemplate raspiRestTemplateBuilder(RestTemplateBuilder builder) {
+       return builder
+               .rootUri(raspiURL)
+               .build();
+    }
+}
