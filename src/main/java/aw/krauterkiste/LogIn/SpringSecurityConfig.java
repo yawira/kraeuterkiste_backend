7577076be0933e83390cd8b8d4a2bd4e9b73f1ea
evgenik <kozyr.e@gmx.de>
2019-08-26 14:03:45 +0200

    Raspi backend implementation starting

--- src/main/java/aw/krauterkiste/LogIn/SpringSecurityConfig.java
+++ src/main/java/aw/krauterkiste/LogIn/SpringSecurityConfig.java
@@ -38,7 +38,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
 //                .antMatchers("/", "/registrierung_neuerUser", "/registrierungAbschliessen", "/loginUser", "/dataview/moisture").permitAll()
-                .antMatchers("/", "/registrierung_neuerUser", "/registrierungAbschliessen", "/loginUser").permitAll()
+                .antMatchers("/", "/registrierung_neuerUser", "/registrierungAbschliessen", "/loginUser","/readImage").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/").loginProcessingUrl("/loginUser").permitAll()
                 .successForwardUrl("/analytics").failureUrl("/")
--- src/main/java/aw/krauterkiste/kraeuterkisteserver/CameraController.java
+++ src/main/java/aw/krauterkiste/kraeuterkisteserver/CameraController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
@@ -42,34 +43,47 @@ public class CameraController {
 //    }
 
 
-    // this method takes the file that was given as a payload by the Client and stores it at a chosen place
-    @CrossOrigin(origins = "http://localhost:5555", allowCredentials = "true")
+   /* // this method takes the file that was given as a payload by the Client and stores it at a chosen place
+    @CrossOrigin(origins = "http://localhost:2020", allowCredentials = "true")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<Object> uploadFile(@RequestParam("file") String file) throws IOException {
         // path where the picture should be saved
-        File convertFile = new File("C:\\Users\\" + file.getOriginalFilename());
+        File convertFile = new File("C:\\Users\\kozyr\\Desktop");
         convertFile.createNewFile();
         FileOutputStream fout = new FileOutputStream(convertFile);
         fout.write(file.getBytes());
         fout.close();
         return new ResponseEntity<>("File is uploaded succressfully aufm Desktop", HttpStatus.OK);
     }
+*/
 
-    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
-    @GetMapping(value = "/readImage")
-    @ResponseBody()
-    public PicturesResponseBody readImage() throws IOException {
+        /*@GetMapping
+        public String getImageFromRaspi () throws IOException {
 
-        PicturesResponseBody picturesResponseBody = new PicturesResponseBody();
+        RestTemplate restTemplate = new RestTemplate();
 
-        Path path = Paths.get("C:\\Users\\IMG_2303.jpg");
-        byte[] picture = Files.readAllBytes(path);
 
-        String encodedImage = Base64.encode(picture);
+        String fileResponse = restTemplate.getForObject("http://localhost:2020/upload",String.class);
+        System.out.println(fileResponse);
+        return fileResponse;
+    }*/
 
-        picturesResponseBody.setEncodedImage(encodedImage);
+        @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+        @GetMapping(value = "/readImage")
+        @ResponseBody()
+        public PicturesResponseBody readImage() throws IOException {
 
-        return picturesResponseBody;
+            PicturesResponseBody picturesResponseBody = new PicturesResponseBody();
+
+            RestTemplate restTemplate = new RestTemplate();
+            String fileResponse = restTemplate.getForObject("http://localhost:2020/upload",String.class);
+            System.out.println(fileResponse);
+
+
+            picturesResponseBody.setEncodedImage(fileResponse);
+            System.out.println("Foto" + fileResponse);
+
+            return picturesResponseBody;
 
     }
 }
