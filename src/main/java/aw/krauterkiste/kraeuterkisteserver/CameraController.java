c38780b0bf9f6bcebcb22a1fef1550c2cb87f7b8
Lorenz Josten <lorenz.josten@gmail.com>
2019-08-27 17:38:53 +0200

    add ConstructorInjection to CameraController add application property values to CameraController

--- src/main/java/aw/krauterkiste/kraeuterkisteserver/CameraController.java
+++ src/main/java/aw/krauterkiste/kraeuterkisteserver/CameraController.java
@@ -3,66 +3,39 @@ package aw.krauterkiste.kraeuterkisteserver;
 import aw.krauterkiste.kraeuterkisteserver.repositories.PicturesRepository;
 import aw.krauterkiste.kraeuterkisteserver.responsebodies.PicturesResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import java.io.*;
 
+import java.io.*;
 
 
 @RestController
 public class CameraController {
 
-    @Autowired
+    @Value("${raspi.url}")
+    private String raspiUrl;
+
     private final PicturesRepository picturesRepository;
 
+    @Autowired
     public CameraController(PicturesRepository picturesRepository) {
         this.picturesRepository = picturesRepository;
     }
 
-    // object returned by the method should be marshalled directly to the HTTP response body
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+    @GetMapping(value = "/readImage")
+    @ResponseBody()
+    public PicturesResponseBody readImage() throws IOException {
 
-//    @PostMapping(value = "/upload")
-//    @ResponseBody
-//    public PicturesResponseBody showPicture() {
-//
-//        PicturesResponseBody picturesResponseBody = new PicturesResponseBody();
-//
-//        picturesResponseBody.setPicturesList(picturesRepository.findAll());
-//
-//        return picturesResponseBody;
-//    }
+        PicturesResponseBody picturesResponseBody = new PicturesResponseBody();
 
+        RestTemplate restTemplate = new RestTemplate();
+        String fileResponse = restTemplate.getForObject(raspiUrl + "/upload", String.class);
 
-   /* // this method takes the file that was given as a payload by the Client and stores it at a chosen place
-    @CrossOrigin(origins = "http://localhost:2020", allowCredentials = "true")
-    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Object> uploadFile(@RequestParam("file") String file) throws IOException {
-        // path where the picture should be saved
-        File convertFile = new File("C:\\Users\\kozyr\\Desktop");
-        convertFile.createNewFile();
-        FileOutputStream fout = new FileOutputStream(convertFile);
-        fout.write(file.getBytes());
-        fout.close();
-        return new ResponseEntity<>("File is uploaded succressfully aufm Desktop", HttpStatus.OK);
-    }
-*/
+        picturesResponseBody.setEncodedImage(fileResponse);
 
-
-
-        @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
-        @GetMapping(value = "/readImage")
-        @ResponseBody()
-        public PicturesResponseBody readImage() throws IOException {
-
-            PicturesResponseBody picturesResponseBody = new PicturesResponseBody();
-
-            RestTemplate restTemplate = new RestTemplate();
-            String fileResponse = restTemplate.getForObject("http://192.168.43.240:7070/upload",String.class);
-
-            picturesResponseBody.setEncodedImage(fileResponse);
-
-            return picturesResponseBody;
-
+        return picturesResponseBody;
     }
 }
 
