d7d71f7e72395eb39b777244fb88264100da7016
Lorenz Josten <lorenz.josten@gmail.com>
2019-08-28 23:23:39 +0200

    fix MoistureController to listen for Messages from pi

--- src/main/java/aw/krauterkiste/moisture/controller/MoistureController.java
+++ src/main/java/aw/krauterkiste/moisture/controller/MoistureController.java
@@ -4,15 +4,11 @@ import aw.krauterkiste.moisture.model.Moisture;
 import aw.krauterkiste.moisture.model.MoistureDto;
 import aw.krauterkiste.moisture.repository.MoistureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 
-@EnableScheduling
 @RestController
 @RequestMapping("/moisture")
 public class MoistureController {
@@ -26,26 +22,11 @@ public class MoistureController {
         this.raspiRestTemplate = raspiRestTemplate;
     }
 
-    @PostConstruct
-    public void startMeasurements() {
-        measureMoisture();
-    }
-
-    @Scheduled(fixedRate = 1000)
-    @GetMapping(value = "/data")
-    public void measureMoisture(){
-
-        Moisture moistureData = new Moisture();
-
-        MoistureDto moistureResponse = raspiRestTemplate.getForObject("/moisture/data", MoistureDto.class);
-
-        moistureData.setMoisturePercentage(moistureResponse.getMoistureData());
-        moistureData.setMoistureDateTime(LocalDateTime.now());
-
-        if(moistureData.getMoisturePercentage() > -1) {
-
-            moistureRepository.save(moistureData);
-        }
-
+    @PostMapping("/data")
+    public void measureMoisture(@ModelAttribute MoistureDto moistureDto){
+        Moisture moisture = new Moisture();
+        moisture.setMoistureDateTime(LocalDateTime.now());
+        moisture.setMoisturePercentage(moistureDto.getMoistureData());
+        moistureRepository.save(moisture);
     }
 }
